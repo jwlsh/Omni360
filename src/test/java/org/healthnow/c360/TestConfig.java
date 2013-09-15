199d765a7fd98a986d5bb8bfbbb9fd003f88d6f7
John Walsh <john.walsh@eaisys.com>
2013-09-14 21:30:43 -0400

    Added Test Classes and Config

---
+++ src/test/java/org/healthnow/c360/TestConfig.java
@@ -0,0 +1,21 @@
+package org.healthnow.c360;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+@EnableNeo4jRepositories(basePackages="com.c360.api.repository")
+public class TestConfig extends Neo4jConfiguration {
+	
+    private static final String DB_PATH = "http://localhost:7474/db/data";
+    
+    @Bean
+    public SpringRestGraphDatabase graphDatabaseService() {
+        return new SpringRestGraphDatabase(DB_PATH);
+    }
+}
